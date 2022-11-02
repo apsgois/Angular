86c8662b7c7d2da3a33b94f3dbe99093d663f53a
apsgois <apsgois@outlook.com>
2022-11-01 22:43:42 -0300

    Event binding

--- data-binding/src/app/data-binding/data-binding.component.ts
+++ data-binding/src/app/data-binding/data-binding.component.ts
@@ -12,8 +12,27 @@ export class DataBindingComponent implements OnInit {
   cursoAngular: boolean = true ;
   urlImage  = 'http://lorempixel.com.br/500/400/?1'
 
+  valorAtual: string = '';
+  valorSalvo: string = '';
+  isMouseOver: boolean = false;
+
   getvalor(){
     return 1;
+  }
+  botaoClicado(){
+    alert('Botão clicado');
+  }
+
+
+  onKeyUp(evento:KeyboardEvent){
+    this.valorAtual=((<HTMLInputElement>evento.target).value);
+  }
+
+  salvarValor(valor:any){
+    this.valorSalvo = valor;
+  }
+  onMouseOverOut(){
+    this.isMouseOver = !this.isMouseOver;
   }
 
   getCurtiCurso(){
